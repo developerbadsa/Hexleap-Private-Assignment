752e44bba46edff52d454f920c95b205455576d0
Rahim Badsa <developerbadsa@gmail.com>
2024-03-22 11:30:02 +0600

    added components and making folder stucture

---
+++ app/Components/Pages_Sections/Sports/SportCard/SportCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SportCard = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default SportCard;
---
+++ app/Components/Pages_Sections/Sports/Sports.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sports = () => {
+  return (
+    <div className='px-2'>
+      <h3 className='border-b-2 inline py-1 border-b-sky-600'>Sports</h3>
+    </div>
+  );
+};
+
+export default Sports;
