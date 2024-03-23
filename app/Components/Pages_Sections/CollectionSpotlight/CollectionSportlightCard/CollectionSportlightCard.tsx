9b1ca811b5273a633a7696d9a5080c8f3bbe4f11
Rahim Badsa <developerbadsa@gmail.com>
2024-03-23 13:19:05 +0600

    Collection Spotlight card done

---
+++ app/Components/Pages_Sections/CollectionSpotlight/CollectionSportlightCard/CollectionSportlightCard.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+
+const CollectionSportlightCard = () => {
+   return (
+      <div className='bg-[#fff] border py-2 shadow-lg'>
+         <div className=' '>
+            <Image
+               alt=''
+               className='w-full'
+               src={'https://i.ibb.co/vvqXxRL/Ticket-Mockup-3-1.png'}
+               width={218}
+               height={0}></Image>
+         </div>
+         {/* Divider */}
+         <div className='p1 border-b-[3px] border-b-gray-300 border-dashed relative mb-5'>
+            <div className='bg-[#F7F7F8] p-4 h-6 w-6 rounded-full absolute -bottom-5 -left-4'></div>
+            <div className='bg-[#F7F7F8] p-4 h-6 w-6 rounded-full absolute -bottom-5 -right-4'></div>
+         </div>
+         <div className='text-center'>
+            <h5 className='my-2 text-lg text-black font-medium'>Las Vegas Aviators</h5>
+            <div className='text-sm'>Oct 15 | Sun | 4:30 PM</div>
+            <p className=' text-sm text-[#525965] mt-2'>Las Vegas Ballpark, Las Vegas, Nevada</p>
+            <button className='px-12 text-white text-xs py-3 bg-black my-3'>Take Flight Collection</button>
+         </div>
+      </div>
+   );
+};
+
+export default CollectionSportlightCard;
--- app/Components/Pages_Sections/CollectionSpotlight/CollectionSpotlight.tsx
+++ app/Components/Pages_Sections/CollectionSpotlight/CollectionSpotlight.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import SectionHeading from '../../Shared/SectionHeading/SectionHeading';
+import CollectionSportlightCard from './CollectionSportlightCard/CollectionSportlightCard';
+
+const sportlightCardData = [1, 2, 3];
 
 const CollectionSpotlight = () => {
    return (
       <section className='mt-8 py-12 px-28'>
-        {/* Heading */}
+         {/* Heading */}
          <div className='text-center '>
-            <SectionHeading 
+            <SectionHeading
                headingText={'Collection Spotlight'}></SectionHeading>
             <p className='text-sm py-4 leading-5 px-10'>
                Discover extraordinary moments with our Spotlight Collection
@@ -14,7 +17,14 @@ const CollectionSpotlight = () => {
                unforgettable experience. Grab yours today!
             </p>
          </div>
-         
+         {/* cards */}
+         <div className='grid grid-cols-3 justify-center gap-7'>
+            {sportlightCardData.map((inx, cardData) => (
+               <CollectionSportlightCard
+                  key={inx}
+                  cardData={cardData}></CollectionSportlightCard>
+            ))}
+         </div>
       </section>
    );
 };
