3950614b38bba18f88974f0cc3ff2caeec95babb
Rahim Badsa <developerbadsa@gmail.com>
2024-03-24 20:22:15 +0600

    sports card data array made with array object and show to ui

--- app/Components/Pages_Sections/Sports/SportCard/SportCard.tsx
+++ app/Components/Pages_Sections/Sports/SportCard/SportCard.tsx
@@ -1,30 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
 
-const SportCard = () => {
+const SportCard = ({cardData}) => {
+   const {title, image, totalEvent, sport} = cardData;
    return (
       <div className='bg-white dark:bg-[#3B3E47] p-2 shadow-lg  dark:text-white'>
          <Image
             alt=''
             className='w-full'
-            src={
-               'https://i.ibb.co/MRRx7tW/277307281-10158751675643224-148609460173032344-n.png'
-            }
+            src={image}
             width={218}
             height={385}></Image>
-         <h4 className='my-2 text-lg font-medium'>
-            Sacromento River Cats
-         </h4>
+         <h4 className='my-2 text-lg font-medium'>{title}</h4>
          <div className=' p-2 flex bg-[#F7F7F8] dark:bg-[#292B32] gap-2 justify-between'>
             <div>
                <span className='text-[#525965] dark:text-[#DFDFDF] text-xs leading-3 '>
                   total events
                </span>
-               <div className='text-sm '>48 Events</div>
+               <div className='text-sm '>{totalEvent}</div>
             </div>
             <div className=''>
-               <span className='text-[#525965] dark:text-[#DFDFDF] text-xs leading-3 '>sport</span>
-               <div className='text-sm'>baseball</div>
+               <span className='text-[#525965] dark:text-[#DFDFDF] text-xs leading-3 '>
+                  sport
+               </span>
+               <div className='text-sm'>{sport}</div>
             </div>
          </div>
       </div>
--- app/Components/Pages_Sections/Sports/Sports.tsx
+++ app/Components/Pages_Sections/Sports/Sports.tsx
@@ -7,9 +7,33 @@ import DarkModeButton from '../../Shared/DarkMode/DarkModeToogle';
 
 const Sports = () => {
    const sportCardDatas = [
-      {
+      {  
+         id:1,
          title: 'Sacramento River Cats',
-         image: 'https://i.ibb.co/MRRx7tW/277307281-10158751675643224-148609460173032344-n.png'
+         image: 'https://i.ibb.co/MRRx7tW/277307281-10158751675643224-148609460173032344-n.png',
+         totalEvent: '48 Events',
+         sport: 'Baseball'
+      },
+      {
+         id: 2,
+         title: 'Las Vegas Aviators',
+         image: 'https://i.ibb.co/jhDYm0g/370265821-812394460893172-8391088016650457893-n-1.png',
+         totalEvent: '28 Events',
+         sport: 'Baseball'
+      },
+      {
+         id: 3,
+         title: 'New jersey devils',
+         image: 'https://i.ibb.co/rQ25mG0/395319316-1098948761258832-1320994362450036961-n-1.png',
+         totalEvent: '15 Events',
+         sport: 'Baseball'
+      },
+      {
+         id:4,
+         title: 'Las Vegas Aviators',
+         image: 'https://i.ibb.co/jhDYm0g/370265821-812394460893172-8391088016650457893-n-1.png',
+         totalEvent: '28 Events',
+         sport: 'Baseball'
       }
    ];
 
@@ -26,8 +50,7 @@ const Sports = () => {
          </div>
          {/*sport cards */}
          <div className='grid lg:grid-cols-5 grid-cols-1 sm:grid-cols-2 gap-5 mt-6 justify-center'>
-            {sportCardDatas.map((inx, cardData) => (
-               <SportCard key={inx}></SportCard>
+            {sportCardDatas.map(( cardData, inx) => (  <SportCard key={inx} cardData={cardData}></SportCard>
             ))}
             <SportsAdCard></SportsAdCard>
          </div>
